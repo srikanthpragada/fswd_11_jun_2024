1e605cf363ef2693ab6cb8ba2a8b52cc4b8cee0c
srikanthpragada <srikanthpragada@gmail.com>
2024-07-03 18:56:08 +0530

    03-JUL

---
+++ js/oop/errors.js
@@ -0,0 +1,13 @@
+try {
+    let cd = new Date()
+    console.log(cd.getMont())
+}
+catch (e) {
+    console.log("Error :" + e)
+}
+finally {
+   console.log("Finally")   
+}
+ 
+console.log("The End")
+ 
