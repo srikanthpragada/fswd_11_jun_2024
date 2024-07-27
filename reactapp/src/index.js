676202b8055a65a9bc1a11b1a494d9f0f710de8f
srikanthpragada <srikanthpragada@gmail.com>
2024-07-27 18:55:29 +0530

    27-JUL

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import InterestForm from './state/InterestForm';
+import UncontrolledDemo from './state/UncontrolledDemo';
 
 var products = [
      {name : 'Bose Speakers', price : 30000}, 
@@ -15,6 +15,6 @@ var products = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-       <InterestForm />
+       <UncontrolledDemo />
   </React.StrictMode>
 );
---
+++ reactapp/src/props/ContextDemo.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+
+const MyContext = React.createContext();
+
+export default function ContextDemo() {
+    const details = {course : 'React', trainer : 'Srikanth Pragada'}
+    return (
+        <MyContext.Provider value={details}>
+            <h1>Parent</h1>
+            <Child />
+        </MyContext.Provider>
+    )
+}
+
+function Child() {
+    const details = useContext(MyContext) // take data from context 
+    return (
+        <>
+            <h2>Child </h2>
+            <h3>Course : {details.course}</h3>
+            <hr />
+            <GrandChild />
+        </>
+    )
+}
+
+function GrandChild() {
+    const details = useContext(MyContext) // take data from context 
+    return (
+        <>
+            <h2>Grand Child </h2>
+            <h3>Course : {details.course} </h3>
+            <h3>Trainer : {details.trainer} </h3>
+        </>
+    )
+}
